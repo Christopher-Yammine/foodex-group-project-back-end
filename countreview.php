99b6034438c65b5b9b44a3be96cb6995f1c97db1
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-30 02:30:59 +0300

    Adding two apis that count,averages and displays reviews and rating

---
+++ countreview.php
@@ -0,0 +1,22 @@
+<?php
+require('connection.php');
+
+$id=$_POST["id"];
+$query =$mysqli->prepare("select count(*) as count ,TRUNCATE(AVG(r.rating),1) as avg,re.restaurant_name,re.restaurant_number  from reviews r join users u on 
+r.user_id=u.id_user join restaurants re on re.id_restaurant=r.id_restaurant where r.id_restaurant=?");
+$query->bind_param("i",$id);
+$query->execute();
+$array = $query->get_result();
+$response =[];
+while ($reviews = $array-> fetch_assoc()){
+    $response[]=$reviews;
+    
+}
+$json = json_encode($response);
+echo $json;
+
+
+
+
+
+?>
---
+++ getreviews.php
@@ -0,0 +1,22 @@
+<?php
+require('connection.php');
+
+$id=$_POST["id"];
+$query =$mysqli->prepare("select r.* ,u.name,re.restaurant_name,re.restaurant_number from reviews r join users u on 
+r.user_id=u.id_user join restaurants re on re.id_restaurant=r.id_restaurant where r.id_restaurant=?");
+$query->bind_param("i",$id);
+$query->execute();
+$array = $query->get_result();
+$response =[];
+while ($reviews = $array-> fetch_assoc()){
+    $response[]=$reviews;
+    
+}
+$json = json_encode($response);
+echo $json;
+
+
+
+
+
+?>
